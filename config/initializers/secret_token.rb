8a8af0fe544dd857faf48e60a7ff794541570533
Isaac Elias <nicecurelom@gmail.com>
2012-12-03 04:40:19 -0800

    initial commit, I think Devise is working, maybe

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Chuck::Application.config.secret_token = 'dae9adc86c0cda45a56172fb29235a578e25986f66ebd048ee11875afbc3d9ca30dfb569f1e26ef3060352505a5c7617fcd02445085022732a9baf3c91f2d015'
