4badba4414717fdc0ce678a083195142e818a8cf
Isaac Elias <nicecurelom@gmail.com>
2012-12-12 23:43:09 -0800

    added devise and FB login

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,6 @@
 Chuck::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
-
+  config.action_mailer.default_url_options = { :host => 'http://otherbook.herokuapp.com/' }
   # Code is not reloaded between requests
   config.cache_classes = true
 
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Chuck::Application.routes.draw do
 
   get "static_pages/about"
 
-  devise_for :users
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
@@ -54,7 +54,7 @@ Chuck::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-   root :to => 'static_pages#index'
+   root :to => 'static_pages#home'
 
   # See how all your routes lay out with "rake routes"
 
